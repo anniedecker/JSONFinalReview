4fdab112b11f72d768ed4218dd6b0d3a584f742a
Annie Decker <anniedecker@ou.edu>
2019-12-08 14:57:21 -0600

    designed form

--- JSONFinalReview/JSONFinalReview/MainWindow.xaml.cs
+++ JSONFinalReview/JSONFinalReview/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*Annie Decker
+* The questions your application need to answer for the user, are as follows:
+* List all of the different genres for the movies
+* Which movie has the highest IMDB score?
+*List all of the different movies that have a number of voted users with 350000 or more
+*How many movies where Anthony Russo is the director?
+*How many movies where Robert Downey Jr. is the actor 1?
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
